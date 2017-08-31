35301ab55dd56e7151f1bb46cdc05ba0b4e0b870
2851827398@qq.com <2851827398@qq.com>
2017-08-31 11:55:15 +0800

    modify register

--- src/front/handlers/user.py
+++ src/front/handlers/user.py
@@ -106,10 +106,10 @@ class LoginHandler(ApiHandler):
                 user_id, username, password_hash, _access_token, _refresh_token = r[0]
                 access_token_redis = self.redis.get('access_token:%s' % access_token)
                 if not access_token_redis:
-                    access_token = binascii.hexlify(os.urandom(20)).decode()
-                    refresh_token = binascii.hexlify(os.urandom(20)).decode()
+                    _access_token = binascii.hexlify(os.urandom(20)).decode()
+                    _refresh_token = binascii.hexlify(os.urandom(20)).decode()
                     query = "UPDATE core_user SET access_token=%s, refresh_token=%s, modified=%s WHERE id=%s"
-                    params = (access_token, refresh_token, int(time.time()))
+                    params = (_access_token, _refresh_token, int(time.time()))
                     for i in range(5):
                         try:
                             yield self.sql.runOperation(query, params)
@@ -117,8 +117,8 @@ class LoginHandler(ApiHandler):
                         except storage.IntegrityError:
                             log.msg("SQL integrity error, retry(%i): %s" % (i, (query % params)))
                             continue
-                self.redis.set('access_token:%s' % access_token, user_id, D.EXPIRATION)
-                self.write(dict(user_id=user_id, access_token=access_token, refresh_token=refresh_token))
+                self.redis.set('access_token:%s' % _access_token, user_id, D.EXPIRATION)
+                self.write(dict(user_id=user_id, access_token=_access_token, refresh_token=_refresh_token))
                 return
             else:
                 self.write(dict(err=E.ERR_USER_PASSWORD, msg=E.errmsg(E.ERR_USER_PASSWORD)))
